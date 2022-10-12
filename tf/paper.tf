68dfd933888834c3d9ef28ba722eca9130d2d119
Kyle <kyle@darklightventures.com>
2022-10-12 09:38:59 -0400

    connecting modules

--- tf/paper.tf
+++ tf/paper.tf
@@ -19,7 +19,17 @@ module "application-loadbalancer" {
 }
 
 module "bastion" {
-    source = "./modules/networking"
+    source = "./modules/bastion"
+    region               = "${var.region}"
+    environment          = "${var.environment}"
+    vpc_cidr             = "${var.vpc_cidr}"
+    public_subnets_cidr  = "${var.public_subnets_cidr}"
+    private_subnets_cidr = "${var.private_subnets_cidr}"
+    availability_zones   = "${local.production_availability_zones}"
+}
+
+module "vpn" {
+    source = "./modules/vpn"
     region               = "${var.region}"
     environment          = "${var.environment}"
     vpc_cidr             = "${var.vpc_cidr}"
