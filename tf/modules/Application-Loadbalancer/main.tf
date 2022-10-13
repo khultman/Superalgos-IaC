a62182df97eb92dbe22849bdd79d4a837c4dab4e
Kyle <kyle@darklightventures.com>
2022-10-13 10:11:53 -0400

    corrected structure; working on alb

--- tf/modules/Application-Loadbalancer/main.tf
+++ tf/modules/Application-Loadbalancer/main.tf
@@ -1,7 +1,6 @@
 
-
 /*==== application load balancer security group ======*/
-resource "aws_security_group" "sg_alb" {
+resource "aws_security_group" "sg_loadbalancer" {
   name        = "${var.environment}-sg-loadbalancer"
   description = "Loadbalancer Security Group"
   vpc_id      = "${module.Networking.vpc_id}"
@@ -21,3 +20,27 @@ resource "aws_security_group" "sg_alb" {
     Team        = "${var.team}"
   }
 }
+
+
+/*==== application load balancer ======*/
+resource "aws_lb" "loadbalancer" {
+  name               = "${var.environment}-loadbalancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_loadbalancer.id]
+  subnets            = [for subnet in ${module.networking.} : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+
