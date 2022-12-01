377ae46d81f32545fc42798bdc4c17a58ca74a0f
Kyle <kyle@darklightventures.com>
2022-11-30 20:47:50 -0500

    implementing a layer strategy for terraform due to a dependency by the vpn module

--- tf/modules/platform/aws/networking/vpn/main.tf
+++ tf/modules/platform/aws/networking/vpn/main.tf
@@ -56,18 +56,18 @@ resource "aws_security_group" "sg_vpn_client" {
 }
 
 
-resource "aws_ec2_client_vpn_network_association" "vpn_subnets" {
-  count = length(var.associated_subnet_ids)
-  client_vpn_endpoint_id  = aws_ec2_client_vpn_endpoint.vpn_client_endpoint.id
-  subnet_id               = var.associated_subnet_ids[count.index]
-  security_groups         = [aws_security_group.sg_vpn_client.id]
-  lifecycle {
-    // The issue why we are ignoring changes is that on every change
-    // terraform screws up most of the vpn assosciations
-    // see: https://github.com/hashicorp/terraform-provider-aws/issues/14717
-    ignore_changes = [subnet_id]
-  }
-}
+# resource "aws_ec2_client_vpn_network_association" "vpn_subnets" {
+#   count = length(var.associated_subnet_ids)
+#   client_vpn_endpoint_id  = aws_ec2_client_vpn_endpoint.vpn_client_endpoint.id
+#   subnet_id               = var.associated_subnet_ids[count.index]
+#   security_groups         = [aws_security_group.sg_vpn_client.id]
+#   lifecycle {
+#     // The issue why we are ignoring changes is that on every change
+#     // terraform screws up most of the vpn assosciations
+#     // see: https://github.com/hashicorp/terraform-provider-aws/issues/14717
+#     ignore_changes = [subnet_id]
+#   }
+# }
 
 
 resource "aws_ec2_client_vpn_authorization_rule" "vpn_auth_rule" {
