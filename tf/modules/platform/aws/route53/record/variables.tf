744a90e4fbd287d0a1f0ed36147c24f0f408dd77
Kyle <kyle@darklightventures.com>
2022-12-08 10:27:20 -0500

    updated variable defaults

--- tf/modules/platform/aws/route53/record/variables.tf
+++ tf/modules/platform/aws/route53/record/variables.tf
@@ -66,11 +66,13 @@ variable "multivalue_answer_routing_policy" {
 }
 
 variable "tags" {
-    description                   = "A mapping of tags to assign to the resource."
-    type                          = map(string)
+  description                     = "A map of tags to assign to resources"
+  type                            = map(string)
+  default                         = {}
 }
 
 variable "tags_for_resource" {
   description                     = "A nested map of tags to assign to specific resource types"
   type                            = map(map(string))
+  default                         = {}
 }
